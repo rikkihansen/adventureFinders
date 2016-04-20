fbb17fc6f60e72f8c7b4508f09c046d133ae1dc1
Rikki Hansen <rikkihansen@Rikkis-MacBook-Air.local>
2016-04-19 20:47:13 -0700

    working on object array

--- adventureFinder.js
+++ adventureFinder.js
@@ -1,5 +1,5 @@
 /// Start of adventrueFinder -----------
-var camp = function (campType, campName, environment, difficulty, amenities, family, pet, activities, latlong) {
+var Camp = function (campType, campName, environment, difficulty, amenities, family, pet, activities, latlong) {
   this.campType= campType;
   this.campName= campName;
   this.environment= environment;
@@ -10,15 +10,17 @@ var camp = function (campType, campName, environment, difficulty, amenities, fam
   this.activities= activites;
   this.latlong= latlong;
 };
+
 // Making empty Array to push camp data into ----------
 var campArray = new Array ();
-campArray.push();
-campArray.push();
-campArray.push();
-campArray.push();
-campArray.push();
-campArray.push();
-campArray.push();
-campArray.push();
-campArray.push();
-campArray.push();
+campArray.push(new Camp());
+campArray.push(new Camp());
+campArray.push(new Camp());
+campArray.push(new Camp());
+campArray.push(new Camp());
+campArray.push(new Camp());
+campArray.push(new Camp());
+campArray.push(new Camp(['Tent', 'Cabin/Yurt', 'RV/Trailer'], "Honeyman Memorial State Park", "Coastal", 0, 3, 4, 2, 1, [43.9272, -124.1086]));
+campArray.push(new Camp(['Tent', 'Cabin/Yurt', 'RV/Trailer'], "Harris Beach State Park", "Coastal", 0, 3, 4, 2, 1, [42.0654, -123.3094]));
+campArray.push(new Camp(['Tent', 'Cabin/Yurt', 'RV/Trailer'], "South Beach State Park", "Coastal", 0, 3, 4, 2, 1, [43.9272, -124.1086]));
+campArray.push(new Camp(['Tent', 'Cabin/Yurt', 'RV/Trailer'], "Sunset Bay State Park", "Coastal", 0, 3, 4, 3, 2, [43.3306, -124.3730]));
