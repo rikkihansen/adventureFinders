50eba6cefcf505165203810294739d696f68303c
cassburt <cass.burt@gmail.com>
2016-04-19 20:55:20 -0700

    environment now only has one value

--- adventureFinder.js
+++ adventureFinder.js
@@ -13,8 +13,8 @@ var Camp = function (campType, campName, environment, difficulty, amenities, fam
 // Making empty Array to push camp data into ----------
 var campArray = new Array ();
 campArray.push(new Camp('Backpacking/Primitive','Jefferson Park', 'Forest', 3, 0, 0, 3, 0, [44.71005, -121.80559]));
-campArray.push(new Camp('Backpacking/Primitive', 'Elk Mountain', ['Forest', 'Mountain',] 3, 0, 0, 3, 0, [45.33468, -121.60756]));
-campArray.push(new Camp('Backpacking/Primitive', 'Grand Valley/Badger Valley', ['Forest', 'Mountain',], 4, 0, 0, 0, 0, [43.2011000, -89.9348500]));
+campArray.push(new Camp('Backpacking/Primitive', 'Elk Mountain', 'Mountain', 3, 0, 0, 3, 0, [45.33468, -121.60756]));
+campArray.push(new Camp('Backpacking/Primitive', 'Grand Valley/Badger Valley', 'Mountain', 4, 0, 0, 0, 0, [43.2011000, -89.9348500]));
 campArray.push(new Camp('Backpacking/Primitive' 'Timberline Trail Near Umbrella Falls', 'Forest', 1, 0, 3, 3, 0, [45.32892, -121.66086]));
 campArray.push();
 campArray.push();
