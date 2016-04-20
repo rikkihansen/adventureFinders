8deb72e8cb82d72ab6ea291a7e943719c408866a
Rikki Hansen <rikkihansen@Rikkis-MacBook-Air.local>
2016-04-19 19:41:37 -0700

    working on js

---
+++ adventureFinder.js
@@ -0,0 +1,24 @@
+/// Start of adventrueFinder -----------
+var camp = function (campType, campName, environment, difficulty, amenities, family, pet, activities, latlong) {
+  this.campType= campType;
+  this.campName= campName;
+  this.environment= environment;
+  this.difficulty= difficulty;
+  this.amenities= amenities;
+  this.family= family;
+  this.pet= pet;
+  this.activities= activites;
+  this.latlong= latlong;
+};
+// Making empty Array to push camp data into ----------
+var campArray = new Array ();
+campArray.push();
+campArray.push();
+campArray.push();
+campArray.push();
+campArray.push();
+campArray.push();
+campArray.push();
+campArray.push();
+campArray.push();
+campArray.push();
