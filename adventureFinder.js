082679ff71584f956188a46900087bdea2e3d84a
cassburt <cass.burt@gmail.com>
2016-04-26 11:39:11 -0700

    New questions and survey method added

--- adventureFinder.js
+++ adventureFinder.js
@@ -1,38 +1,39 @@
 /// Start of adventrueFinder -----------
-var Camp = function(campName, carCamp, rv, cabin, backpack, forest, mountain, coast, difficulty, amenities, family, pet, activities, latlong) {
+var Camp = function(campName, comfort, isolation, accessibility, pet, family, activities, areaType, latlong) {
     this.campName = campName;
-    this.carCamp = carCamp;
-    this.rv = rv;
-    this.cabin = cabin;
-    this.backpack = backpack;
-    this.forest = forest;
-    this.mountain = mountain;
-    this.coastal = coast;
-    this.difficulty = difficulty;
-    this.amenities = amenities;
-    this.family = family;
+    this.comfort = comfort;
+    this.isolation = isolation;
+    this.accessibility = accessibility;
     this.pet = pet;
+    this.family = family;
     this.activities = activities;
+    this.areaType = areaType;
     this.latlong = latlong;
 };
 
 // Making empty Array to push camp data into ----------
 var campArray = new Array();
-campArray.push(new Camp('Jefferson Park', false, false, false, true, true, false, false, 3, 0, 0, 3, 0, [44.71005, -121.80559]));
-campArray.push(new Camp('Elk Mountain', false, false, false, true, false, true, false, 3, 0, 0, 3, 0, [45.33468, -121.60756]));
-campArray.push(new Camp('Grand Valley/Badger Valley', false, false, false, true, false, true, false, 4, 0, 0, 0, 0, [47.898, -123.369]));
-campArray.push(new Camp('Timberline Trail Near Umbrella Falls', false, false, false, true, true, false, false, 1, 0, 3, 3, 0, [45.32892, -121.66086]));
-campArray.push(new Camp("Lost Lake", true, false, true, false, false, true, false, 1, 2, 4, 1, 3, [45.4900, -121.8223]));
-campArray.push(new Camp("Olallie Lake", false, false, true, false, false, true, false, 1, 4, 4, 2, 3, [44.807903, -121.788338]));
-campArray.push(new Camp("LaPine State Park", true, true, true, false, true, false, false, 0, 4, 4, 3, 4, [43.4606, -121.3048]));
-campArray.push(new Camp("Beverly Beach", true, true, true, false, false, false, true, 0, 4, 4, 3, 4, [44.7205, -121.0560]));
-campArray.push(new Camp("Honeyman Memorial State Park", true, true, true, false, false, false, true, 0, 3, 4, 2, 1, [43.9272, -124.1086]));
-campArray.push(new Camp("Harris Beach State Park", true, true, true, false, false, false, true, 0, 3, 4, 2, 1, [42.0654, -123.3094]));
-campArray.push(new Camp("South Beach State Park", true, true, true, false, false, false, true, 0, 3, 4, 2, 1, [43.9272, -124.1086]));
-campArray.push(new Camp("Sunset Bay State Park", true, true, true, false, false, false, true, 0, 3, 4, 3, 2, [43.3306, -124.3730]));
-campArray.push(new Camp("Tumalo State Park", true, true, true, false, true, false, false, 1, 3, 4, 3, 4, [44.1288, -121.3309]));
-campArray.push(new Camp("Iron Creek", true, true, false, false, true, false, false, 1, 2, 2, 2, 2, [46.1746, -122.7746]));
+campArray.push(new Camp('Jefferson Park', 0, 3, 1, 2, 0, 0, "forest", [44.71005, -121.80559]));
+campArray.push(new Camp('Elk Mountain', 0, 2, 2, 3, 1, 0, "mountain", [45.33468, -121.60756]));
+campArray.push(new Camp('Grand Valley/Badger Valley', 0, 3, 0, 1, 1, 0, "forest", [43.2011000, -89.9348500]));
+campArray.push(new Camp('Timberline Trail Near Umbrella Falls', 0, 3, 0, 0, 0, 0, "forest", [45.32892, -121.66086]));
+campArray.push(new Camp("Lost Lake", 1, 1, 3, 1, 3, 3, "forest", [45.4900, -121.8223]));
+campArray.push(new Camp("Olallie Lake", 1, 2, 1, 2, 2, 2, "mountain", [44.807903, -121.788338]));
+campArray.push(new Camp("LaPine State Park", 2, 1, 2, 1, 2, 3, "forest", [43.4606, -121.3048]));
+campArray.push(new Camp("Beverly Beach", 2, 2, 2, 1, 3, 3, "coastal", [29.5147, -81.1445]));
+campArray.push(new Camp("Honeyman Memorial State Park", 1, 1, 3, 2, 2, 2, "coastal", [43.9272, -124.1086]));
+campArray.push(new Camp("Harris Beach State Park", 1, 0, 3, 2, 2, 3, "coastal", [42.0654, -123.3094]));
+campArray.push(new Camp("South Beach State Park", 2, 0, 3, 3, 3, 2, "coastal", [43.9272, -124.1086]));
+campArray.push(new Camp("Sunset Bay State Park", 3, 1, 2, 0, 2, 0, "coastal", [43.3306, -124.3730]));
 
+//Creates counters for campsite properties; used to determine the filter value in the finder survey
+var comfortCounter = 0;
+var isolationCounter = 0;
+var accessibilityCounter = 0;
+var petCounter = 0;
+var familyCounter = 0;
+var activitiesCounter = 0;
+var areaCounter = 0;
 
 //Make new array to 'clones' campArray, use to splice out results ------------------
 var availableCamp = new Array();
@@ -51,31 +52,15 @@ function questionFilter(property, propertyValue) {
     });
 }
 
-//Removes all objects with a value equal to or less than the selected input--------------------------
-function radioScale(inputName, property) {
-    var radios = document.getElementsByName(inputName);
-    for (var i = 0; i < radios.length; i++) {
-        radios[i].addEventListener('click', function() {
-            for (var y = parseInt(this.value) - 1; y >= 0; y--) {
-                questionFilter(property, y);
-            }
-            console.log(availableCamp);
-            listenAction();
-        });
-    }
-
-}
-
-
-//Removes all objects with a "false" value for the object property selected--------------------------------
-function radioTag(inputID, property) {
+//Removes all objects with a "false" value for the object property selected, and increases the counter for that property
+function buttonBinder(inputID, property, counter) {
     var radios = document.getElementById(inputID);
     radios.addEventListener('click', function() {
-        questionFilter(property, false);
+    questionFilter(property, counter);
+        console.log(availableCamp.length);
         console.log(availableCamp);
-        listenAction();
+    listenAction();
     });
-
 }
 
 //listener Functions
@@ -95,7 +80,6 @@ function listenAction() {
     }
 }
 
-
 // displaying questions------------------------------------------
 var x = 0;
 
@@ -105,7 +89,6 @@ function displayQuestion() {
         var questionNumber = document.getElementById(radioId);
         questionNumber.style.display = "block";
     }
-
 
 // show and hiding questions on Finder page --------------------------
 function removeQuestion() {
@@ -118,18 +101,33 @@ function removeQuestion() {
 //Functions that fun on pageload---------------------------------
 window.onload = function() {
     getCampResults();
-    radioTag('in1a', 'carCamp');
-    radioTag('in1b', 'rv');
-    radioTag('in1c', 'cabin');
-    radioTag('in1d', 'backpack');
-    radioTag('in2a', 'mountain');
-    radioTag('in2b', 'coastal');
-    radioTag('in2c', 'forest');
-    radioScale('question3', 'amenities');
-    radioScale('question4', 'difficulty');
-    radioScale('question5', 'family');
-    radioScale('question6', 'pet');
-    radioScale('question7', 'activities');
+    buttonBinder('comfort0', 'comfort', comfortCounter++);
+    buttonBinder('comfort1', 'comfort', comfortCounter++);
+    buttonBinder('comfort2', 'comfort', comfortCounter++);
+    buttonBinder('comfort3', 'comfort', comfortCounter++);
+    buttonBinder('isolation0', 'isolation', isolationCounter++);
+    buttonBinder('isolation1', 'isolation', isolationCounter++);
+    buttonBinder('isolation2', 'isolation', isolationCounter++);
+    buttonBinder('isolation3', 'isolation', isolationCounter++);
+    buttonBinder('accessibility0', 'accessibility', accessibilityCounter++);
+    buttonBinder('accessibility1', 'accessibility', accessibilityCounter++);
+    buttonBinder('accessibility2', 'accessibility', accessibilityCounter++);
+    buttonBinder('accessibility3', 'accessibility', accessibilityCounter++);
+    buttonBinder('activities0', 'activities', activitiesCounter++);
+    buttonBinder('activities1', 'activities', activitiesCounter++);
+    buttonBinder('activities2', 'activities', activitiesCounter++);
+    buttonBinder('activities3', 'activities', activitiesCounter++);
+    buttonBinder('family0', 'family', familyCounter++);
+    buttonBinder('family1', 'family', familyCounter++);
+    buttonBinder('family2', 'family', familyCounter++);
+    buttonBinder('family3', 'family', familyCounter++);
+    buttonBinder('pet0', 'pet', petCounter++);
+    buttonBinder('pet1', 'pet', petCounter++);
+    buttonBinder('pet2', 'pet', petCounter++);
+    buttonBinder('pet3', 'pet', petCounter++);
+    buttonBinder('forest', 'forest', areaCounter++);
+    buttonBinder('coastal', 'coastal', areaCounter++);
+    buttonBinder('mountain', 'mountain', areaCounter++);
 }
 
 // initialize and add interactive Leafletjs map centered on Oregon with custom icons
@@ -148,7 +146,6 @@ var myIcon = L.icon({
 });
 
 var markers = new L.layerGroup(); //groups all camp markers into a group to ease placement and removal
-
 
 //function that places markers on latlong in campArray objects
 function addMarkers() {
