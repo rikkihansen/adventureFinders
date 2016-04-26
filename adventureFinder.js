17cd6aee7579db843d6db040be778888e4017544
cassburt <cass.burt@gmail.com>
2016-04-25 20:42:05 -0700

    new questions, constructor, and array objects

--- adventureFinder.js
+++ adventureFinder.js
@@ -1,35 +1,30 @@
 /// Start of adventrueFinder -----------
-var Camp = function(campName, carCamp, rv, cabin, backpack, forest, mountain, coast, difficulty, amenities, family, pet, activities, latlong) {
+var Camp = function(campName, comfort, isolation, accessibility, pet, family, activities, areaType, latlong) {
     this.campName = campName;
-    this.carCamp = carCamp;
-    this.rv = rv;
-    this.cabin = cabin;
-    this.backpack = backpack;
-    this.forest = forest;
-    this.mountain = mountain;
-    this.coastal = coast;
-    this.difficulty = difficulty;
-    this.amenities = amenities;
-    this.family = family;
+    this.comfort = comfort;
+    this.isolation = isolation;
+    this.accessibility = accessibility;
     this.pet = pet;
+    this.family = family;
     this.activities = activities;
+    this.areaType = areaType;
     this.latlong = latlong;
 };
 
 // Making empty Array to push camp data into ----------
 var campArray = new Array();
-campArray.push(new Camp('Jefferson Park', false, false, false, true, true, false, false, 3, 0, 0, 3, 0, [44.71005, -121.80559]));
-campArray.push(new Camp('Elk Mountain', false, false, false, true, false, true, false, 3, 0, 0, 3, 0, [45.33468, -121.60756]));
-campArray.push(new Camp('Grand Valley/Badger Valley', false, false, false, true, false, true, false, 4, 0, 0, 0, 0, [43.2011000, -89.9348500]));
-campArray.push(new Camp('Timberline Trail Near Umbrella Falls', false, false, false, true, true, false, false, 1, 0, 3, 3, 0, [45.32892, -121.66086]));
-campArray.push(new Camp("Lost Lake", true, false, true, false, false, true, false, 1, 2, 4, 1, 3, [45.4900, -121.8223]));
-campArray.push(new Camp("Olallie Lake", false, false, true, false, false, true, false, 1, 4, 4, 2, 3, [44.807903, -121.788338]));
-campArray.push(new Camp("LaPine State Park", true, true, true, false, true, false, false, 0, 4, 4, 3, 4, [43.4606, -121.3048]));
-campArray.push(new Camp("Beverly Beach", true, true, true, false, false, false, true, 0, 4, 4, 3, 4, [29.5147, -81.1445]));
-campArray.push(new Camp("Honeyman Memorial State Park", true, true, true, false, false, false, true, 0, 3, 4, 2, 1, [43.9272, -124.1086]));
-campArray.push(new Camp("Harris Beach State Park", true, true, true, false, false, false, true, 0, 3, 4, 2, 1, [42.0654, -123.3094]));
-campArray.push(new Camp("South Beach State Park", true, true, true, false, false, false, true, 0, 3, 4, 2, 1, [43.9272, -124.1086]));
-campArray.push(new Camp("Sunset Bay State Park", true, true, true, false, false, false, true, 0, 3, 4, 3, 2, [43.3306, -124.3730]));
+campArray.push(new Camp('Jefferson Park', 0, 3, 1, 2, 0, 0, "forest", [44.71005, -121.80559]));
+campArray.push(new Camp('Elk Mountain', 0, 2, 2, 3, 1, 0, "mountain", [45.33468, -121.60756]));
+campArray.push(new Camp('Grand Valley/Badger Valley', 0, 3, 0, 1, 1, 0, "forest", [43.2011000, -89.9348500]));
+campArray.push(new Camp('Timberline Trail Near Umbrella Falls', 0, 3, 0, 0, 0, 0, "forest", [45.32892, -121.66086]));
+campArray.push(new Camp("Lost Lake", 1, 1, 3, 1, 3, 3, "forest", [45.4900, -121.8223]));
+campArray.push(new Camp("Olallie Lake", 1, 2, 1, 2, 2, 2, "mountain", [44.807903, -121.788338]));
+campArray.push(new Camp("LaPine State Park", 2, 1, 2, 1, 2, 3, "forest", [43.4606, -121.3048]));
+campArray.push(new Camp("Beverly Beach", 2, 2, 2, 1, 3, 3, "coastal", [29.5147, -81.1445]));
+campArray.push(new Camp("Honeyman Memorial State Park", 1, 1, 3, 2, 2, 2, "coastal", [43.9272, -124.1086]));
+campArray.push(new Camp("Harris Beach State Park", 1, 0, 3, 2, 2, 3, "coastal", [42.0654, -123.3094]));
+campArray.push(new Camp("South Beach State Park", 2, 0, 3, 3, 3, 2, "coastal", [43.9272, -124.1086]));
+campArray.push(new Camp("Sunset Bay State Park", 3, 1, 2, 0, 2, 0, "coastal", [43.3306, -124.3730]));
 
 //Make new array to 'clones' campArray, use to splice out results ------------------
 var availableCamp = new Array();
@@ -60,15 +55,15 @@ function radioScale(inputName, property) {
             listenAction();
         });
     }
-
 }
-
 
 //Removes all objects with a "false" value for the object property selected--------------------------------
 function radioTag(inputID, property) {
     var radios = document.getElementById(inputID);
     radios.addEventListener('click', function() {
-        questionFilter(property, false);
+    var test = JSON.parse(this.value);
+    questionFilter(property, test);
+        console.log(availableCamp.length);
         console.log(availableCamp);
         listenAction();
     });
@@ -115,18 +110,33 @@ function removeQuestion() {
 //Functions that fun on pageload---------------------------------
 window.onload = function() {
     getCampResults();
-    radioTag('in1a', 'carCamp');
-    radioTag('in1b', 'rv');
-    radioTag('in1c', 'cabin');
-    radioTag('in1d', 'backpack');
-    radioTag('in2a', 'mountain');
-    radioTag('in2b', 'coastal');
-    radioTag('in2c', 'forest');
-    radioScale('question3', 'amenities');
-    radioScale('question4', 'difficulty');
-    radioScale('question5', 'family');
-    radioScale('question6', 'pet');
-    radioScale('question7', 'activities');
+    radioTag('comfort0', 'comfort');
+    radioTag('comfort1', 'comfort');
+    radioTag('comfort2', 'comfort');
+    radioTag('comfort3', 'comfort');
+    radioTag('isolation0', 'isolation');
+    radioTag('isolation1', 'isolation');
+    radioTag('isolation2', 'isolation');
+    radioTag('isolation3', 'isolation');
+    radioTag('accessibility0', 'accessibility');
+    radioTag('accessibility1', 'accessibility');
+    radioTag('accessibility2', 'accessibility');
+    radioTag('accessibility3', 'accessibility');
+    radioTag('activities0', 'activities');
+    radioTag('activities1', 'activities');
+    radioTag('activities2', 'activities');
+    radioTag('activities3', 'activities');
+    radioTag('family0', 'family');
+    radioTag('family1', 'family');
+    radioTag('family2', 'family');
+    radioTag('family3', 'family');
+    radioTag('pet0', 'pet');
+    radioTag('pet1', 'pet');
+    radioTag('pet2', 'pet');
+    radioTag('pet3', 'pet');
+    radioTag('forest', 'forest');
+    radioTag('coastal', 'coastal');
+    radioTag('mountain', 'mountain');
 }
 
 // initialize and add interactive Leafletjs map centered on Oregon with custom icons
